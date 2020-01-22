ea23c047303dfe97c99f9cc9603a406fe3a6118c
milos-prince <michael@freedomgopher.com>
2020-01-22 11:35:24 -0500

    selector partially complete

--- src/components/heroSlider.js
+++ src/components/heroSlider.js
@@ -5,12 +5,14 @@ import postPic2 from '../images/postPic2.jpg'
 import postPic3 from '../images/postPic3.jpg'
 
 
+
 const Hero = () => (
   <section className="setMaxWidth heroBG d-flex">
+   
     <div className="heroSelector d-flex">
-      <Post picSelection={postPic}/>
-      <Post picSelection={postPic2}/>
-      <Post picSelection={postPic3}/>
+      <div className="selectorWrapper"><Post picSelection={postPic}/></div>
+      <div className="selectorWrapper"><Post picSelection={postPic2} /></div>
+     <div className="selectorWrapper"><Post picSelection={postPic3} /></div> 
     </div>
   </section>
   
